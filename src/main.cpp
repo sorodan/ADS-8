985843bc4c033d71a053de4e4ab0981ca10327ca
sorodan <alekseimetelkow@gmail.com>
2026-06-06 00:12:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
+#include <string>
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> tree;
+    const char* filename = "war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
     return 0;
 }
